314e94e7d066ae74d5afb6dcb98f8753f51e24dc
Lampard190 <65217262+Lampard190@users.noreply.github.com>
2020-06-26 00:50:00 +0400

    JS правки

--- js/modal.js
+++ js/modal.js
@@ -1,33 +1,32 @@
 var searchHotel = document.querySelector(".button-search-of-hotel");
-var modalWin = document.querySelector(".modal");
-var form = document.querySelector(".button-search");
-var datearr = document.querySelector("[name=date_of_arrival]");
-var datedep = document.querySelector("[name=date_of_departure]");
-var amountInp = document.querySelector("[name=amount]");
+var modalWin = document.querySelector(".modal-show");
+var searchForm = document.querySelector(".button-search");
+var date = document.querySelector("input[type=text]");
+var amountInp = document.querySelector("input[type=number]");
 var storage = localStorage.getItem("amountInp");
 
 searchHotel.addEventListener("click", function () {
    	modalWin.classList.toggle('modal-none');
-   	modalWin.classList.toggle('modal-show');
-   	datearr.focus();
+   	date.focus();
    	if (storage) {
    		amountInp.value = storage;
    	}
    });
 
-   	form.addEventListener("submit", function (evt){
-   		if (!datearr.value || !datedep.value || !amountInp.value) {
+   	searchForm.addEventListener("submit", function (evt) {
+   		if (!date.value && !amountInp.value) {
    		evt.preventDefault();
    		console.log("Нужно ввести дату и количество");
    	} else {
    		localStorage.setItem("amountInp", amountInp.value);
    	}	
-   });
+  });
 
-   	window.addEventListener("keydown" function(evt) {
-   		if (evt.keyCode === 27 {
-   			if (modalWin.classList.contains"modal-show")) {
-   			evt.preventDefault();
-   			modalWin.classList.remove("modal-show");
+   	window.addEventListener("keydown", function(evt) {
+   		if (evt.keyCode === 27) {
+   			if (modalWin.classList.contains("modal-show")) {
+          evt.preventDefault();
+         	modalWin.classList.remove("modal-show");
+        }   
    		}
    	});
